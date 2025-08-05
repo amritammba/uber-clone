a3f1fbdab2e712cae8d28a0d079283503e9998b2
amritammba <amritarao26046@gmail.com>
2025-08-05 23:40:38 +0530

    fourth
    connectivity issue fixed in postman for sending data(post)

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,21 +1,79 @@
-const userModel = require('../models/user.model'); 
+// const userModel = require('../models/user.model');
+// const userService = require('../services/user.service');
+// const { validationResult } = require('express-validator');
+// const blackListTokenModel = require('../models/blackListToken.model');
+
+// module.exports.registerUser = async (req, res, next) => {
+
+//     const errors = validationResult(req);
+//     if (!errors.isEmpty()) {
+//         return res.status(400).json({ errors: errors.array() });
+//     }
+
+//     const { firstname,lastname, email, password } = req.body;
+
+//     const isUserAlready = await userModel.findOne({ email });
+
+//     if (isUserAlready) {
+//         return res.status(400).json({ message: 'User already exist' });
+//     }
+
+//     const hashedPassword = await userModel.hashPassword(password);
+
+//     const user = await userService.createUser({
+//         firstname: fullname.firstname,
+//         lastname: fullname.lastname,
+//         email,
+//         password: hashedPassword
+//     });
+
+//     const token = user.generateAuthToken();
+
+//     res.status(201).json({ token, user });
+
+
+// }
+
+
+
+
+const userModel = require('../models/user.model');
 const userService = require('../services/user.service');
 const { validationResult } = require('express-validator');
 
-module.exports.registerUser = async (req, res,next) => { 
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
-    }
-    const { firstname, lastname, email, password } = req.body;
-    const hashedPassword = await userModel.hashPassword(password);
-    const user = await userService.createUser({ 
-        firstname, 
-        lastname, 
-        email, 
-        password: hashedPassword 
-    });
-    const token = user.generateAuthToken();
-    res.status(201).json({token,user});
+module.exports.registerUser = async (req, res, next) => {
+    try {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
 
+        // Extract from req.body.fullname
+        const { fullname, email, password } = req.body;
+        const { firstname, lastname } = fullname || {};
+
+        // Check if user already exists
+        const isUserAlready = await userModel.findOne({ email });
+        if (isUserAlready) {
+            return res.status(400).json({ message: 'User already exist' });
+        }
+
+        // Hash the password
+        const hashedPassword = await userModel.hashPassword(password);
+
+        // Create the user
+        const user = await userService.createUser({
+            firstname,
+            lastname,
+            email,
+            password: hashedPassword
+        });
+
+        // Generate JWT token
+        const token = user.generateAuthToken();
+
+        res.status(201).json({ token, user });
+    } catch (err) {
+        res.status(500).json({ message: 'Internal server error', error: err.message });
+    }
 }
