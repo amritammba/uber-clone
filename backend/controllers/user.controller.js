041640a8a026d0c33bc2a49a827f01d6bf9cc96f
amritammba <amritarao26046@gmail.com>
2025-08-08 15:55:47 +0530

    User login
    User login completed in postman

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -77,3 +77,27 @@ module.exports.registerUser = async (req, res, next) => {
         res.status(500).json({ message: 'Internal server error', error: err.message });
     }
 }
+
+
+module.exports.loginUser = async (req, res, next) => {  
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const {email, password} = req.body;
+
+    const user = await userModel.findOne({email}).select('+password');
+    if(!user){
+        return res.status(401).json({message: 'Invalid email or password'});
+    }
+
+    const isMatch = await user.comparePassword(password);
+    if(!isMatch){
+        return res.status(401).json({message: 'Invalid email or password'});
+    }
+
+    const token = user.generateAuthToken();
+    res.status(200).json({token, user});
+//4937
+}
